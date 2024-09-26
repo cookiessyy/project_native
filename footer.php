6191767a2d199f3be808c7c004b9d362932c262a
Ganang <rbfamilysmd@gmail.com>
2024-09-26 16:28:52 +0800

    babi2

--- footer.php
+++ footer.php
@@ -1,35 +1,26 @@
-<footer class="bg-gray-900 text-gray-300 py-8">
+<footer class="bg-gray-100 text-gray-700 py-8">
     <div class="container mx-auto px-6">
         <div class="flex flex-wrap justify-between">
             <!-- Left side: Logo and Social media -->
             <div class="w-full md:w-1/4 mb-6">
-                <img src="pict/logo2.png" alt="Logo" class="mb-4 w-24 h-auto">
-                <p class="mb-4">Making the world a better place through constructing elegant hierarchies.</p>
+                <img src="pict/icon.png" alt="Logo" class="mb-4 w-24 h-auto">
+                <p class="mb-4">Dinas Kebudayaan, Kepemudaan, Olahraga, Dan Parawisata Kota Banjarmasin </p>
                 <div class="flex space-x-4">
-                    <a href="#" class="hover:text-gray-500">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                            <!-- Facebook Icon -->
-                        </svg>
-                    </a>
-                    <a href="#" class="hover:text-gray-500">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                             <!-- Instagram Icon -->
-                        </svg>
+                    <a href="https://www.instagram.com/budporaparbjm/?hl=id" class="hover:text-gray-600">
+                        <img src="pict/instagramlogo.png" alt="Instagram" class="w-8 h-8">
                     </a>
-                    <a href="#" class="hover:text-gray-500">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                            <!-- Twitter/X Icon -->
-                        </svg>
+                            <!-- Facebook Icon -->
+                    <a href="https://web.facebook.com/budporaparbjm/?_rdc=1&_rdr" class="hover:text-gray-600">
+                        <img src="pict/facebooklogo.png" alt="Facebook" class="w-8 h-8">
                     </a>
-                    <a href="#" class="hover:text-gray-500">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                            <!-- GitHub Icon -->
-                        </svg>
+                            <!-- Twitter Icon -->
+                            <a href="https://twitter.com/budparbjm" class="hover:text-gray-600">
+                        <img src="pict/xlogo.png" alt="x" class="w-8 h-8">
                     </a>
-                    <a href="#" class="hover:text-gray-500">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                            <!-- YouTube Icon -->
-                        </svg>
+                            <!-- Youtube Icon -->
+                    <a href="https://www.youtube.com/channel/UCoCwWPlc0d4Xg-xSxKly3Aw" class="hover:text-gray-600">
+                        <img src="pict/youtubelogo.png" alt="x" class="w-11 h-8">
                     </a>
                 </div>
             </div>
@@ -37,7 +28,7 @@
             <!-- Right side: Links Section -->
             <div class="w-full md:w-3/4 flex justify-around">
                 <div>
-                    <h3 class="text-gray-400 font-semibold mb-3">Solutions</h3>
+                    <h3 class="text-gray-600 font-semibold mb-3">Solutions</h3>
                     <ul class="space-y-2">
                         <li><a href="404page.php" class="hover:text-blue-500">Marketing</a></li>
                         <li><a href="404page.php" class="hover:text-green-500">Analytics</a></li>
@@ -46,7 +37,7 @@
                     </ul>
                 </div>
                 <div>
-                    <h3 class="text-gray-400 font-semibold mb-3">Support</h3>
+                    <h3 class="text-gray-600 font-semibold mb-3">Support</h3>
                     <ul class="space-y-2">
                         <li><a href="pricing.php" class="hover:text-red-500">Pricing</a></li>
                         <li><a href="404page.php" class="hover:text-yellow-500">Documentation</a></li>
@@ -55,7 +46,7 @@
                     </ul>
                 </div>
                 <div>
-                    <h3 class="text-gray-400 font-semibold mb-3">Company</h3>
+                    <h3 class="text-gray-600 font-semibold mb-3">Company</h3>
                     <ul class="space-y-2">
                         <li><a href="about.php" class="hover:text-red-600">About</a></li>
                         <li><a href="blog.php" class="hover:text-yellow-600">Blog</a></li>
@@ -65,7 +56,7 @@
                     </ul>
                 </div>
                 <div>
-                    <h3 class="text-gray-400 font-semibold mb-3">Legal</h3>
+                    <h3 class="text-gray-600 font-semibold mb-3">Legal</h3>
                     <ul class="space-y-2">
                         <li><a href="404page.php" class="hover:text-blue-600">Claim</a></li>
                         <li><a href="404page.php" class="hover:text-pink-600">Privacy</a></li>
@@ -76,7 +67,7 @@
         </div>
 
         <!-- Footer bottom -->
-        <div class="mt-8 text-center text-gray-500 border-t border-gray-700 pt-4">
+        <div class="mt-8 text-center text-gray-500 border-t border-gray-300 pt-4">
             &copy; 2024 Cookies Tech, Inc. All rights reserved.
         </div>
     </div>
